c76dc96032d9413253c53d187c41e6c65d194a00
ASky <mc_master47@outlook.com>
2025-05-02 16:44:14 -0700

    Beginning to write the fakehack stuff

---
+++ bin/cb2/fakehack.ps1
@@ -0,0 +1,7 @@
+Clear-Host
+Write-Host "`nFake Hack is a CLI-ONLY module, switching to CLI UI type..."
+Start-Sleep -Seconds 2
+Clear-Host
+Write-Host "ProTip: Press F11 to enter full screen. `nAlso, go to Settings > System > For Developers > Terminal, and choose Windows Terminal. `nThen, go to Terminal Settings > Default profile > Appearance > Scroll Bar and make it Hidden."
+Pause
+.\bin\modules\fh\fhstart.ps1
