b62d2d09435daed894adc6d70e299b520d9b70db
ASky <mc_master47@outlook.com>
2025-05-03 23:19:05 -0700

    updated version number

--- TB.ps1
+++ TB.ps1
@@ -4,7 +4,7 @@ function Restart {
     .\bin\reset.ps1
     $Global:comstatDirectX = (Get-WmiObject -Class Win32_VideoController | Select-Object -First 1).DriverVersion
     $Global:UiType = Get-Content -Path "bin\ui.config"
-    $Global:appversionid = "v0.01.00332-beta"
+    $Global:appversionid = "v0.01-beta"
     .\bin\alias.ps1
 }
 Restart
