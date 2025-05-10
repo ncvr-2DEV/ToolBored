c26ad1ecc75fcf297f2a7cd49a91cf8898832879
ASky <mc_master47@outlook.com>
2025-05-10 00:04:03 -0700

    updated version id

--- TB.ps1
+++ TB.ps1
@@ -3,7 +3,7 @@ $host.UI.RawUI.WindowTitle = "ToolBored starting..."
 function Restart {
     .\bin\reset.ps1
     $Global:comstatDirectX = (Get-WmiObject -Class Win32_VideoController | Select-Object -First 1).DriverVersion
-    $Global:appversionid = "v0.01.00380-beta"
+    $Global:appversionid = "v0.02-stable"
     .\bin\alias.ps1
 }
 Restart
