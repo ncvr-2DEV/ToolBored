98f0c2b3fbd70ce002edfeb07fa88265f257b3d2
ASky <mc_master47@outlook.com>
2025-05-09 23:45:06 -0700

    !

--- update.ps1
+++ update.ps1
@@ -2,4 +2,10 @@ $buildInfo = Get-Content -Path .\bin\buildinfo.json -Raw | ConvertFrom-Json
 
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/eiedouno/ToolBored/stable/installer/bin/initiate.ps1" -OutFile .\bin\initiate-update.ps1
 
-.\bin\initiate-update.ps1 -Path $buildInfo.Path -Version $buildInfo.Version -License "true" -Start "true"
+Start-Process -FilePath ".\bin\initiate-update.ps1" -ArgumentList @(
+    "-Path $($buildInfo.Path)",
+    "-Version $($buildInfo.Version)",
+    "-License", "true",
+    "-Start", "true"
+) 
+Stop-Process -Id $PID
